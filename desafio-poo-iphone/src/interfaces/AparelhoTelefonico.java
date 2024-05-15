da5d2a1646fb7ce2147c8a70b9065f55c213ae26
vitor277 <vitor-gasques@hotmail.com>
2024-05-15 09:54:17 -0300

    interfaces criadas

---
+++ desafio-poo-iphone/src/interfaces/AparelhoTelefonico.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public interface AparelhoTelefonico {
+
+    public void ligar(int numero);
+    public void atender();
+    public void iniciarCorreioVoz();
+    
+    
+}
---
+++ desafio-poo-iphone/src/interfaces/NavegadorInternet.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface NavegadorInternet {
+
+    public void exibirPagina(String nomePagina);
+    public void adicionarNovaAba();
+    public void atualizaPagina();
+    
+}
